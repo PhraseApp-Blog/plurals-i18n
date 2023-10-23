36cc36fe100530e769936e814c29b9ca97f7c20b
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-10-23 18:13:22 +0100

    feat: add basic i18n

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 
-const message_en = "🌳 We've planted {count} trees so far!";
 const initialValue = 23456789;
-
-input.addEventListener("input", () => {
-  output.textContent = message_en.replace("{count}", input.value);
-});
-
 input.value = initialValue;
-input.dispatchEvent(new InputEvent("input"));
+input.addEventListener("input", render);
+
+function render() {
+  document.querySelector("#lblCount").textContent = i18next.t("count_label");
+  document.querySelector("#lblMessage").textContent =
+    i18next.t("message_label");
+
+  output.textContent = i18next.t("message", { count: input.value });
+}
+
+function changeLanguage(lang) {
+  i18next.changeLanguage(lang);
+  document.lang = lang;
+  document.dir = lang === "ar" ? "rtl" : "ltr";
+  render();
+}
+
+i18next
+  .init({
+    lng: "en",
+    debug: true,
+    resources: {
+      en: {
+        translation: {
+          count_label: "Count",
+          message_label: "Message",
+          message: "🌳 We've planted {{count}} trees so far!",
+        },
+      },
+      ar: {
+        translation: {
+          count_label: "العدد",
+          message_label: "الرسالة",
+          message: "🌳 لقد زرعنا {{count}} شجرة حتى الآن!",
+        },
+      },
+    },
+  })
+  .then((_) => {
+    render();
+  })
+  .catch(console.error);
